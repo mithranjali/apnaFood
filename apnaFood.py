349bf722c4432906cf34780b9f6b27189de416f3
sripriya.maturi <sripriya.maturi@cropin.com>
2023-02-22 13:00:58 +0530

    testing

--- apnaFood.py
+++ apnaFood.py
@@ -12,7 +12,7 @@ def hello_world():
         return 'Hello World'
 
 @app.get('/sms_otp')
-async def send_sms_otp(phnnum: str = Query(..., max_length=50, min_length=3)):
+async def send_sms_otp():
         return {"hi":10}
         otp=randint(1000,9999)
         params = {'apikey': 'NTA2NDZhNjgzNDVhNGYzNTZiMzE2YTczNDQ2YzYxNzk=', 'numbers': phnnum, 'message' : 'Welcome to apnaFood by MITHRANJALI FOUNDATION.\nYour OTP for registration is ' + str(otp) + '.', 'sender': 'MTRJLI'}
