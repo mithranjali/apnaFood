a70c5ffb1f2d5d18a9da50d1e2426919f2d43863
sripriya.maturi <sripriya.maturi@cropin.com>
2023-02-22 12:39:49 +0530

    made sms fuction async

--- apnaFood.py
+++ apnaFood.py
@@ -12,7 +12,7 @@ def hello_world():
         return 'Hello World'
 
 @app.get('/sms_otp')
-def send_sms_otp(phnnum):
+async def send_sms_otp(phnnum):
     otp=randint(1000,9999)
     params = {'apikey': 'NTA2NDZhNjgzNDVhNGYzNTZiMzE2YTczNDQ2YzYxNzk=', 'numbers': phnnum, 'message' : 'Welcome to apnaFood by MITHRANJALI FOUNDATION.\nYour OTP for registration is ' + str(otp) + '.', 'sender': 'MTRJLI'}
     f = urllib.request.urlopen('https://api.textlocal.in/send/?'+ urllib.parse.urlencode(params))
