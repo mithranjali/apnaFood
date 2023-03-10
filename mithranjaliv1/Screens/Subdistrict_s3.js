c25ed6d09bcfbe344a974cf8cd3b3d650e3a0bd6
Nikhil1357 <lakshmim1357@gmail.com>
2023-03-11 00:55:15 +0530

    "reason for rejection text field" added in SubDistrict Screen 3

--- mithranjaliv1/Screens/Subdistrict_s3.js
+++ mithranjaliv1/Screens/Subdistrict_s3.js
@@ -2,16 +2,23 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable space-infix-ops */
 import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import { Text, View, TouchableOpacity, Alert, ScrollView, Modal } from 'react-native';
 import { StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 
 const Subdistrict_s3 = ({navigation, route}) => {
 
     const data = route.params.data;
+    const [showmodal,set_showmodal] = useState(false);
+    const [rejectreason,set_rejectreason] = useState('');
+    const [rejectactive, set_rejectactive] = useState(true);
+    const [approveactive, set_approveactive] = useState(true);
 
+    console.log(rejectreason);
     const backend_approve = () =>
     {
+
         console.log('Approved');
         fetch('http://apnafood.org.in/sub_district_accept?email='+data.email)
         .then(fdata=>fdata.json())
@@ -20,13 +27,16 @@ const Subdistrict_s3 = ({navigation, route}) => {
                 if (fdata==='accepted by sub district franchisee')
                 {
                     Alert.alert('User : "'+data.username + '" is Approved');
+                    set_approveactive(false);
                 }
             }
             )
-        .catch(err=>console.log(err));
+        .catch(()=>Alert.alert('Please retry'));
     };
     const backend_reject = () =>
     {
+        set_showmodal(false);
+        console.log(rejectreason);
         console.log('Rejected');
         fetch('http://apnafood.org.in/sub_district_reject?email='+data.email)
         .then(fdata=>fdata.json())
@@ -34,15 +44,42 @@ const Subdistrict_s3 = ({navigation, route}) => {
             {
                 if (fdata==='rejected by sub district franchisee')
                 {
-                    Alert.alert('User : "'+data.username + '" is Rejected')
+                    Alert.alert('User : "'+data.username + '" is Rejected');
+                    set_rejectactive(false);
                 }
             }
             )
-        .catch(err=>console.log(err));
+        .catch(()=>Alert.alert('Please retry'));
     };
 
     return (
     <View style={styles.Container}>
+        <Modal
+        animationType="fade"
+        transparent={true}
+        visible={showmodal}
+        >
+            <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Enter the reason of rejection !</Text>
+            <TextInput
+            onChangeText={(text) => set_rejectreason(text)}
+            multiline={true}
+            numberOfLines={2}
+                style={styles.input_reason}
+            />
+            <TouchableOpacity
+            onPress={()=>{
+                backend_reject();
+            }}
+            >
+                <View style={styles.button_reason}>
+                    <Text>Reject</Text>
+                </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </Modal>
         <ScrollView
             showsVerticalScrollIndicator={false}
             style={{
@@ -101,9 +138,20 @@ const Subdistrict_s3 = ({navigation, route}) => {
         </ScrollView>
         <View style={styles.buttons_container}>
         <TouchableOpacity
+        disabled={!approveactive}
         onPress={()=>{backend_approve();}}
         >
-            <View style={styles.button1}>
+            <View style={{
+                backgroundColor:'#8EC63E',
+                opacity:approveactive?1:0.8,
+                color:'#f5f5f5',
+                width:'100%',
+                alignItems:'center',
+                height:50,
+                paddingHorizontal:'16%',
+                justifyContent: 'center',
+                borderRadius:6,
+            }}>
                 <Text
                 style={{
                     fontFamily:'didactgothic-regular',
@@ -115,9 +163,24 @@ const Subdistrict_s3 = ({navigation, route}) => {
             </View>
         </TouchableOpacity>
         <TouchableOpacity
-        onPress={()=>{backend_reject()}}
+        disabled={!rejectactive}
+        onPress={
+                ()=>{
+                    // backend_reject();
+                    set_showmodal(true);
+                    }
+                }
         >
-            <View style={styles.button2}>
+            <View style={{
+                backgroundColor:'red',
+                opacity:rejectactive?1:0.8,
+                width:'100%',
+                height:50,
+                paddingHorizontal:'16%',
+                alignItems:'center',
+                justifyContent: 'center',
+                borderRadius:6,
+            }}>
             <Text
                 style={{
                     fontFamily:'didactgothic-regular',
@@ -140,7 +203,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         // padding:'6%',
-        backgroundColor:'#f5f5f5'
+        backgroundColor:'#f5f5f5',
         // backgroundColor:'red',
     },
     detail:{
@@ -156,27 +219,27 @@ const styles = StyleSheet.create({
         alignContent:'space-between',
         justifyContent: 'space-evenly',
     },
-    button1:{
-        backgroundColor:'#8EC63E',
-        color:'#f5f5f5',
-        // padding:'12%',
-        width:'100%',
-        alignItems:'center',
-        height:50,
-        paddingHorizontal:'16%',
-        justifyContent: 'center',
-        borderRadius:6,
-    },
-    button2:{
-        backgroundColor:'red',
-        // padding:'12%',
-        width:'100%',
-        height:50,
-        paddingHorizontal:'16%',
-        alignItems:'center',
-        justifyContent: 'center',
-        borderRadius:6,
-    },
+    // button1:{
+    //     backgroundColor:'#8EC63E',
+    //     color:'#f5f5f5',
+    //     // padding:'12%',
+    //     width:'100%',
+    //     alignItems:'center',
+    //     height:50,
+    //     paddingHorizontal:'16%',
+    //     justifyContent: 'center',
+    //     borderRadius:6,
+    // },
+    // button2:{
+    //     backgroundColor:'red',
+    //     // padding:'12%',
+    //     width:'100%',
+    //     height:50,
+    //     paddingHorizontal:'16%',
+    //     alignItems:'center',
+    //     justifyContent: 'center',
+    //     borderRadius:6,
+    // },
     headingmain:{
         fontFamily:'didactgothic-regular',
         letterSpacing:0.7,
@@ -223,7 +286,59 @@ const styles = StyleSheet.create({
         borderBottomColor:'#999999',
         borderBottomWidth:StyleSheet.hairlineWidth,
         width:'100%',
-    }
+    },
+    //modal
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 12,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 4,
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+      input_reason:{
+        // borderColor:'green',
+        // borderWidth:1,
+        borderBottomColor:'black',
+        borderBottomWidth:2,
+        borderRadius:4,
+        width:160,
+        // height:60,
+        margin:20,
+      },
+      button_reason:{
+        backgroundColor:'#8cc7e2',
+        // backgroundColor:'#8399cf',
+        // padding:'12%',
+        width:'100%',
+        height:40,
+        paddingHorizontal:'8%',
+        alignItems:'center',
+        justifyContent: 'center',
+        borderRadius:6,
+        },
+        modalText:{
+            color:'black',
+            fontSize:16,
+        },
 });
 
 export default Subdistrict_s3;
